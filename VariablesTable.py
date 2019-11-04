d65a8a0905b37d91cbf15b5bf4bdbd44cacc914c
Irvel <irvel.nduva@gmail.com>
2019-11-04 10:19:31 -0600

    Use addresses instead of var names

--- VariablesTable.py
+++ VariablesTable.py
@@ -1,30 +1,35 @@
 class VariablesTable(object):
     """docstring for VariablesTable"""
     def __init__(self):
-        self._symbols = {}
+        self._vars = {}
+        self._curr_address = 0
 
-    def search(self, name):
-        if name in self._symbols:
-            return self._symbols[name]
+    def search(self, address):
+        if address in self._vars:
+            return self._vars[address]
         else:
             return None
 
-    def add_symbol(self, symbol):
-        self._symbols[symbol.name] = symbol
+    def add_var(self, variable):
+        self._vars[variable.address] = variable
+
+    def new_address(self):
+        self._curr_address += 1
+        return self._curr_address
 
 
 class Function(object):
     """docstring for Function"""
-    def __init__(self, name, ret_type, ret_value):
+    def __init__(self, name, ret_type, address):
         self.name = name
         self.ret_type = ret_type
-        self.ret_value = ret_value
+        # self.ret_value = ret_value
+        self.address = address
 
 
 class Variable(object):
     """docstring for Variable"""
-    def __init__(self, name, data_type, value, scope):
+    def __init__(self, name, data_type, address):
         self.name = name
         self.data_type = data_type
-        self.value = value
-        self.scope = scope
+        self.address = address
