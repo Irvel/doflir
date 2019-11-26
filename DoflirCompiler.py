3ae700ec8b674838fb8d59cad14d909a8bc389b5
Irvel <irvel.nduva@gmail.com>
2019-11-26 02:51:27 -0600

    Hide debug logs with a program arg flag

--- DoflirCompiler.py
+++ DoflirCompiler.py
@@ -14,6 +14,7 @@ def read_input():
     parser = argparse.ArgumentParser(description="Doflir Compiler")
     parser.add_argument("in_file",
                         type=str, help="Filename of Doflir program to compile")
+    parser.add_argument("--debug", action="store_true")
     args = parser.parse_args()
     print(f"Compiling {args.in_file}...\n")
     input_file = open(args.in_file, "r")
@@ -32,19 +33,20 @@ def write_bytecode(bytecode, filename):
 
 
 def main():
-    in_filename, input_code = read_input()
-    bytecode = doflir_compile(input_code)
+    in_filename, input_code, debug = read_input()
+    bytecode = doflir_compile(in_filename, input_code, debug)
     out_filename = make_obj_filename(in_filename)
     write_bytecode(bytecode, out_filename)
-    print("\n" + input_code)
+    if debug:
+        print("\n" + input_code)
     print(f'Saved bytecode to "{out_filename}"!')
 
 
-def doflir_compile(input_code):
+def doflir_compile(in_filename, input_code, debug):
     lexer = DoflirLexer(antlr4.InputStream(input_code))
     tokens = antlr4.CommonTokenStream(lexer)
     parser = DoflirParser(tokens)
-    visitor = DoflirCustomVisitor()
+    visitor = DoflirCustomVisitor(in_filename, input_code, debug)
 
     # parser.removeErrorListeners()
     # self.error = io.StringIO()
